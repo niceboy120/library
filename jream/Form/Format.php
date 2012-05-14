41777dacd383827a8b4e18fe6c133f8a263ac42b
Jesse Boyer <aniyishay@gmail.com>
2012-05-14 19:51:51 -0300

    Update jream/Form/Format.php

--- jream/Form/Format.php
+++ jream/Form/Format.php
@@ -18,6 +18,26 @@ class Format
 	 * @param string $param
 	 * 
 	 * @return string
+	 * <?php
+/**
+ * @author		Jesse Boyer <contact@jream.com>
+ * @copyright	Copyright (C), 2011-12 Jesse Boyer
+ * @license		GNU General Public License 3 (http://www.gnu.org/licenses/)
+ *				Refer to the LICENSE file distributed within the package.
+ *
+ * @link		http://jream.com
+ * @category	Form
+ */
+namespace jream\Form;
+class Format
+{
+	/**
+	 * call - Run any PHP function to format
+	 * 
+	 * @param string $call
+	 * @param string $param
+	 * 
+	 * @return string
 	 * 
 	 * @throws Exception Upon invalid function
 	 */
